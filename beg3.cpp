f02796661462782a554079a29421bdee745e1233
ajayjose123 <53247551+ajayjose123@users.noreply.github.com>
2019-07-24 11:23:00 +0530

    Create beg3.cpp

---
+++ beg3.cpp
@@ -0,0 +1,19 @@
+    
+#include<iostream>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+  char a;
+  cin>>a;
+  if(isalpha(a))
+  {
+    if(a == 'a'||a == 'A'||a == 'e'||a == 'E'||a == 'i'||a == 'I'||a == 'o'||a == 'O'||a == 'u'|| a == 'U')
+      cout<<"Vowel";
+    else
+      cout<<"Consonant";
+  }
+  else
+    cout<<"Invalid";
+  return 0;
+}
