8232afc8ff80f63ed5c83c4a50b5fed008002c5b
ajayjose123 <53247551+ajayjose123@users.noreply.github.com>
2019-07-25 15:53:26 +0530

    Create b12.cpp

---
+++ b12.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+  int n, digits = 0, cpy, rev;
+  cin>>n;
+  cpy = n;
+  
+  while(cpy!=0)
+  {
+    cpy/=10;
+    digits++;
+  }
+  
+  for(int i = 0; i<digits; i++)
+  {
+    r = n%10;
+    rev += r * pow(10, (digits - i));
+  }
+  
+  if(n == rev)
+  cout<<"yes";
+  else
+  cout<<"no";
+  
+  return 0;
+}
