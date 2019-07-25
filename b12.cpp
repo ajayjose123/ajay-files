50439a576eb61c6c0df12d4e8100302ea030882f
ajayjose123 <53247551+ajayjose123@users.noreply.github.com>
2019-07-25 21:10:10 +0530

    Update b12.cpp

--- b12.cpp
+++ b12.cpp
@@ -7,6 +7,8 @@ int main()
   int n, digits = 0, cpy, rev = 0, r;
   cin >> n;
   
+  cpy = n;
+  
   while(cpy!=0)
   {
     cpy/=10;
@@ -17,7 +19,7 @@ int main()
   for(int i = 1; i<=digits; i++)
   {
     r=cpy%10;
-    rev += r*pow(10, digits - i));
+    rev += r*pow(10, (digits - i));
     cpy/=10;
   }
   
