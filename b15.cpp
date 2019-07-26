0970afe55448c9aa92fdcadacd8aae7b216db8f1
ajayjose123 <53247551+ajayjose123@users.noreply.github.com>
2019-07-26 20:28:28 +0530

    Update b15.cpp

--- b15.cpp
+++ b15.cpp
@@ -1,2 +1,16 @@
 #include<iostream>
-using name
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cin>>a>>b;
+  
+  for(int i=a+1; i<b; i++)
+  {
+    if(i%2 == 0)
+      cout<<i<<" ";
+  }
+  
+  return 0;
+}
