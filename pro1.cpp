a89e6b9cd20e68c20eb9d89b6f3e96d4b3c2e868
ajayjose123 <53247551+ajayjose123@users.noreply.github.com>
2019-07-29 22:01:18 +0530

    Update pro1.cpp

--- pro1.cpp
+++ pro1.cpp
@@ -5,6 +5,41 @@ using namespace std;
 
 int main()
 {
-  string n;
+  int n;
+  cin>>n;
   
+  string a[n];
+  string result = NULL;
+  
+  bool eql = true;
+  
+  for(int i=0; i<n; i++)
+    cin>>a[i];
+  
+  string cpy = a;
+  
+  for(int i=0; i<cpy.length(); i++)
+  {
+    for(int j=1; j<n; j++)
+    {
+      if(cpy[i] != a[j][i])
+      {
+        eql = false;
+        break;
+      }
+    }
+    
+    if(eql == true)
+      result.append(cpy[i]);
+    else
+    {
+      if(result != NULL)
+        cout<<result;
+      else
+        cout<<"No common prefix";
+      break;
+    }
+  }
+  
+  return 0;
 }
