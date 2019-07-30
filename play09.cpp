3d66f484a24642c0068832f9b32e7cda335a02c2
ajayjose123 <53247551+ajayjose123@users.noreply.github.com>
2019-07-30 12:24:22 +0530

    Update play09.cpp

--- play09.cpp
+++ play09.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
+using namespace std;
 int main()
 {
   int l,r,f=0,count=0;
   cin>>l>>r;
   for ( int i=l; i<=r;i++)
   {
-    for (int j=0; i<(i/2); j++)
+    for (int j=2; j<=(i/2); j++)
     {
         if( i%j==0)
-          f++;
+          {f++; break;}
      }
-     if (f==1)
+     if (f==0)
       count++;
+      f=0;
   }
   cout<<count;
 }
