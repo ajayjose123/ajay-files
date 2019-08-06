34c20bb7997a2da6fc43f179c80761c91bdd2062
ajayjose123 <53247551+ajayjose123@users.noreply.github.com>
2019-08-06 09:49:36 +0530

    Create pro57.cpp

---
+++ pro57.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  string a, b;
+  cin>>a>>b;
+  
+  bool present = false;
+  
+  for(int i=0; i<a.length(); i++)
+  {
+    for(int j=0; j<b.length(); j++)
+    {
+      if(a[i] == b[j])
+      {
+        if(a[i+1] == b[j+1])
+        {
+          present = true;
+          break;
+        }
+      }
+    }
+    
+    if(present == true)
+        break;
+  }
+  
+  if(present == true)
+      cout<<"yes";
+    else
+      cout<<"no";
+}
